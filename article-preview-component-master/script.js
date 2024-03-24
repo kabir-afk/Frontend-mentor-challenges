81e6ff6f398843eca58db5bce186b02993fa513b
kabir-afk <kabirsingh12b@gmail.com>
2024-03-24 17:47:20 +0530

    removed unnecessary explanation

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -4,14 +4,6 @@ const shareSvgPath = document.querySelector("#share-svg-path");
 
 shareButton.addEventListener("click", () => {
   shareButton.classList.toggle("darken");
-  // if (shareButton.classList.contains('darken')) {
-  //     shareSvg.firstElementChild.style.fill = lightGrayishBlue
-  // }
-  // else{
-  //     shareSvg.firstElementChild.style.fill = desaturatedDarkGrayishBlue
-  // }
-
-  // same code but shorter, feels better , tried toggling class inside svg but didnt work so manipulated thru js
   shareSvgPath.style.fill = shareButton.classList.contains(
     "darken"
   )
